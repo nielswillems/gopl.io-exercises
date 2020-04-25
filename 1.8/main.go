8155f83f1acc322a498c134531268414360ef801
Niels Willems <niels.willems88@gmail.com>
2020-04-25 20:20:26 +0200

    Exercise 1.8

---
+++ 1.8/main.go
@@ -0,0 +1,30 @@
+// Exercise 1.8 Fetch prints the content found at each specified URL, adds http prefix when not present.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	const httpProtocol = "http://"
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, httpProtocol) {
+			url = httpProtocol + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
